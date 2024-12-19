389b3001eace6979b6453fa5675a8de6ed8eb462
Marharyta <rita.shyshkavets@gmail.com>
2024-12-19 09:35:44 +0100

    feat(extended-1.2): add alternative text for dynamically added images

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement = function(taskString){
 
     deleteButton.className = "section__btn section__btn--delete";
     deleteButtonImg.src = './remove.svg';
+    deleteButtonImg.alt = "remove";
     deleteButton.appendChild(deleteButtonImg);
 
 
